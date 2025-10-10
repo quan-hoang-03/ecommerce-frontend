916669df20f167bc4599f0e785c79af7f245b084
Quân Hoàng <quanhoang789a@gmail.com>
2025-10-11 01:41:31 +0700

    ghép api cart và order

--- src/customer/State/Products/Action.js
+++ src/customer/State/Products/Action.js
@@ -1,16 +1,30 @@
 import { api } from "../../../config/apiConfig";
 
 export const findProduct=(reqData)=>async(dispatch)=>{
-    dispatch({ type: "FIND_PRODUCTS_REQUEST" });
-    const {color,size,minPrice,maxPrice,minDiscount,category,stock,sort,pageNumber,pageSize}=reqData;
-    try {
-        const { data } = api.get(
-          `/api/products/color=${color}&size=${size}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`
-        );
-        dispatch({type:"FIND_PRODUCTS_SUCCESS",payload:data})
-    } catch (error) {
-        dispatch({type:"FIND_PRODUCTS_FAILURE",payload:error.message})
-    }
+  //(dispatch) => { ... }: Redux Thunk cho phép bạn trả về một hàm, chứ không chỉ là một object.
+  //Hàm này nhận vào dispatch — là hàm dùng để gửi (dispatch) một action đến Redux store.
+  //dispatch giúp bạn “bắn tín hiệu” cho reducer biết rằng có sự kiện (action) vừa xảy ra, để reducer cập nhật state tương ứng
+  dispatch({ type: "FIND_PRODUCTS_REQUEST" });
+  const {
+    color,
+    size,
+    minPrice,
+    maxPrice,
+    minDiscount,
+    category,
+    stock,
+    sort,
+    pageNumber,
+    pageSize,
+  } = reqData;
+  try {
+    const { data } = api.get(
+      `/api/products/color=${color}&size=${size}&minPrice=${minPrice}&maxPrice=${maxPrice}&minDiscount=${minDiscount}&category=${category}&stock=${stock}&sort=${sort}&pageNumber=${pageNumber}&pageSize=${pageSize}`
+    );
+    dispatch({ type: "FIND_PRODUCTS_SUCCESS", payload: data });
+  } catch (error) {
+    dispatch({ type: "FIND_PRODUCTS_FAILURE", payload: error.message });
+  }
 }
 
 export const findProductById = (reqData) => async (dispatch) => {
--- src/customer/State/store.js
+++ src/customer/State/store.js
@@ -1,11 +1,21 @@
 import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
+//Nếu không có thunk, Redux sẽ báo lỗi vì dispatch chỉ nhận object, không nhận function.
 import { thunk } from "redux-thunk";
 import authReducer from "./Auth/Reducer";
 import { customerProductReducer } from "./Products/Reducer";
+import { cartReduce } from "./Cart/Reducer";
+import { orderReducer } from "./Order/Reducer";
 
+//Mỗi reducer sẽ phụ trách một phần riêng của state
 const rootReducer = combineReducers({
-    auth: authReducer,
-    product:customerProductReducer,
+  //Quản lý trạng thái đăng nhập, token, user info
+  auth: authReducer,
+  //Quản lý danh sách sản phẩm, tìm kiếm, lọc, chi tiết sản phẩm
+  product: customerProductReducer,
+  //Quản lý giỏ hàng: thêm, xoá, cập nhật sản phẩm
+  cart: cartReduce,
+  //
+  order: orderReducer,
 });
 
 const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
